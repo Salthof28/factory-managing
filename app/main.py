ca2a081c90437fc1ea3e45ef1edaf4a707832951
Salman <salmanalthof31@gmail.com>
2026-02-28 11:50:54 +0700

    feat: register patch users

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+from app.users import users_router
 from app.warehouse.routes import warehouse_router
 # from app.warehouse.routes import WarehouseController
 
 app = FastAPI()
 # warehouseController = WarehouseController()
 
-app.include_router(warehouse_router, prefix="/warehouse", tags=["warehouse"])
+app.include_router(warehouse_router, prefix="/warehouse", tags=["Warehouse"])
+app.include_router(users_router, prefix="/users", tags=["Users"])
